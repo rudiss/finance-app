59e8503af39599ddee1829a20543492068c418b2
Rudinei SIlva <sistema3@comunicareaparelhosauditivos.com>
2016-08-04 00:08:44 -0300

    friend view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,15 @@
 class UsersController < ApplicationController
 
   def my_portfolio
+
       @user_stocks = current_user.stocks
       @user = current_user
+
   end
 
-def my_friends
+        def my_friends
+
+        @friendships = current_user.friends
 
         end
 
