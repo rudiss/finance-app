45e30b0640d47f027a98f5e7674d95ca69083126
Rudinei SIlva <sistema3@comunicareaparelhosauditivos.com>
2016-08-03 22:05:27 -0300

    nav-bar, css, and etc...

--- app/controllers/user/registrations_controller.rb
+++ app/controllers/user/registrations_controller.rb
@@ -1,11 +1,14 @@
-class User::ResgisrationsController < Devise::RegistrationsController
-  before_filter :configue_permitted_parametrs
+class User::RegistrationsController < Devise::RegistrationsController
+
+  before_filter :configure_permitted_parameters
 
   protected
 
-  def configue_permitted_parametrs
-    devise_parameter_sanitizer.for(:sign_up).push(:first_name, :last_name)
-    devise_parameter_sanitizer.for(:account_update).push(:first_name, :last_name)
+  def configure_permitted_parameters
+
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :last_name])
 
   end
+
 end
